cd76e1e6a2b4d754d7d919ab53cca568768d83ec
Vladimir Suvorov <bluurn@gmail.com>
2026-08-03 17:30:49 +0200

    Add root route that redirects to the first community alphabetically

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,4 +1,9 @@
 class CommunitiesController < ApplicationController
+  def index
+    community = Community.order(:name).first!
+    redirect_to community_path(community.slug)
+  end
+
   def show
     @community = Community.find_by!(slug: params[:slug])
     @selections = @community.selections.includes(:content).order("contents.title")
